a9fd40f2f7248410e4d9b8a406bf803aa3f814b4
Max Pittore <pittore@gfz-potsdam.de>
2014-06-17 20:49:05 +0200

    updated SamplingPoints.R

--- R/SamplingPoints.R
+++ R/SamplingPoints.R
@@ -8,5 +8,8 @@ function(rast,coef)
 	#rescale the points on the raster extent
 	x<-Z$x*((mbox[1,2]-mbox[1,1])/ndens$xrange[2])+mbox[1,1]
 	y<-(Z$y)*((mbox[2,2]-mbox[2,1])/ndens$yrange[2])+mbox[2,1]
-	return(SpatialPoints(cbind(x,y)))
+	out<-SpatialPoints(cbind(x,y))
+	#re-set original projection
+	crs(out)<-crs(rast)
+	return(SpatialPointsDataFrame(out,as.data.frame(out)))
 }
